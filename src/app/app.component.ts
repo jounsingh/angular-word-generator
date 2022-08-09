ea48cf9b2c2c111442c0afe708d7f8f77e28768a
jounsingh <jounpratapsingh9@gmail.com>
2022-08-09 17:34:31 +0530

    projectcomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWord from "../utils/words"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+  word ='';
+  wordLimit = 10;
+
+  handleLimitChange = (limit:Event) => {
+    
+    this.wordLimit = parseInt((limit.target as HTMLInputElement).value)
+  }
+
+  generate = () => {
+    this.word = arrayWord.slice(0,this.wordLimit).join(" ");
+  }
 }
